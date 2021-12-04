c7d3d2fe073ac4532a837fcbd290d64d5fb4163c
Caner <kurucaner@gmail.com>
2021-12-04 18:58:56 -0500

    ifelse

---
+++ ifelse.rb
@@ -0,0 +1,30 @@
+# if/else
+
+# condition = false
+# another_condition = false
+
+# if condition && another_condition
+#   puts "Hello"
+# else
+#   puts "Goodbye"
+# end
+
+# # puts "Lalala"
+
+# if !condition || another_condition
+#   puts "this evaluated to true"
+# else
+#   puts "this evaluated to false"
+# end
+
+# name = "Caner"
+
+# if name == "John"
+#   puts "Hello John"
+# elsif name == "Paul"
+#   puts "Hello Paul"
+# elsif name == "George"
+#   puts "Hello George"
+# else
+#   puts "Who are you?"
+# end
--- playground.rb
+++ playground.rb
@@ -17,6 +17,23 @@ puts "Enter the first number"
 num_1 = gets.chomp
 puts "Enter the second number"
 num_2 = gets.chomp
-puts "The first number multiplied by the second number is: #{multiply(num_1, num_2)}"
-puts "The second number diveded by the first number is: #{divide(num_1, num_2)}"
-puts "The third number added by the second number is: #{add(num_2, num_1)}"
+# puts "The first number multiplied by the second number is: #{multiply(num_1, num_2)}"
+# puts "The second number diveded by the first number is: #{divide(num_1, num_2)}"
+# puts "The third number added by the second number is: #{add(num_2, num_1)}"
+
+puts "What do you want to do?"
+puts "1) Multiply"
+puts "2) Divide"
+puts "3) Add"
+
+user_input = gets.chomp
+
+if user_input == "1"
+    puts "The first number multiplied by the second number is: #{multiply(num_1, num_2)}"
+    elsif user_input == "2"
+    puts "The second number diveded by the first number is: #{divide(num_1, num_2)}"
+    elsif user_input == "3"
+    puts "The third number added by the second number is: #{add(num_2, num_1)}"
+    else
+    puts "Invalid input"
+end
